89fa5f4f25e6570d5a4ef4d1ee3bb6603be93eeb
Nesreen <79583526+nesreenKhalid-flextock@users.noreply.github.com>
2025-06-17 14:00:35 +0300

    .

--- group_ordering_app/settings.py
+++ group_ordering_app/settings.py
@@ -1,3 +1,7 @@
+import os
+import dj_database_url
+
+
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -7,4 +11,16 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'group_ordering_app',
 ]
+
 ALLOWED_HOSTS = ['*']
+# Supabase PostgreSQL Database Configuration
+DATABASE_URL="postgresql://postgres.uxedjorelcqsirkwlwev:[89QzEX6bs5Vnr_+]@aws-0-eu-west-2.pooler.supabase.com:5432/postgres"
+
+# Database configuration
+DATABASES = {
+    'default': dj_database_url.config(
+        default=os.environ.get(DATABASE_URL),
+        conn_max_age=600,
+        conn_health_checks=True,
+    )
+}
