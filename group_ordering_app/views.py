62bcf4d06a84798decb66b4ec5ac88de11543f7a
muhammadsherief24 <muhammad.sherief@flextock.com>
2025-06-17 14:13:47 +0300

    Add initial Django project structure and configuration files

--- group_ordering_app/views.py
+++
@@ -1,6 +0,0 @@
-
-from django.shortcuts import render
-from django.http import HttpResponse
-
-def home(request):
-    return HttpResponse("<h1>Welcome to Group Ordering App!</h1><p>Your Django app is running successfully on Replit.</p>")
