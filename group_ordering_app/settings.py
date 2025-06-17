831a3e4be437c42837d73e18180cc0e0f9d6c292
muhammadsherief24 <muhammad.sherief@flextock.com>
2025-06-17 14:04:44 +0300

    fix minor issues

--- group_ordering_app/settings.py
+++ group_ordering_app/settings.py
@@ -14,12 +14,12 @@ INSTALLED_APPS = [
 
 ALLOWED_HOSTS = ['*']
 # Supabase PostgreSQL Database Configuration
-DATABASE_URL="postgresql://postgres.uxedjorelcqsirkwlwev:[89QzEX6bs5Vnr_+]@aws-0-eu-west-2.pooler.supabase.com:5432/postgres"
+DATABASE_URL="postgresql://postgres.uxedjorelcqsirkwlwev:89QzEX6bs5Vnr_+@aws-0-eu-west-2.pooler.supabase.com:5432/postgres"
 
 # Database configuration
 DATABASES = {
     'default': dj_database_url.config(
-        default=os.environ.get(DATABASE_URL),
+        default=DATABASE_URL,
         conn_max_age=600,
         conn_health_checks=True,
     )
